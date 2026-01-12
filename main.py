5ca659b8502e0c41eab7b4c9b617d2e05bb1958e
ranjeetsingh03-oss <ranjeetsingh03@gmail.com>
2026-01-11 17:38:24 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-n=17
-print("The Table of 17:")
-for i in range(1,11):
-    m=n*i
-    print(m)
-
-
-num=1
-sum=0
-while (num<=10):
-    sum=sum+num
-    num=num+1
-    print(sum)
-
- number=29
-flag=False
-if number>1:
-    for i in range(2,number):
-        if (number%i)==0:
-            flag=True
-            break
-        if flag:
-            print(number,"is not a prime number")
-            else:
-            print(number,"is a prime number")
+a=2
+b=4
+print(a**b)
